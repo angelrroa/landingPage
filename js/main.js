18a4dce286fa629b0b1b6f80cec853ad6956f526
angelrroa <67547536+angelrroa@users.noreply.github.com>
2024-01-18 13:52:09 -0500

    barra de navegación y audio al clickear imagen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 AOS.init({
     offset:1
 });
+
+let nav = document.querySelector('nav');
+document.querySelector('.boton').addEventListener('click',function(){
+    nav.classList.toggle('active')
+});
+
+var porsche911Audio = document.getElementById('porsche911Audio');
+var imgPorsche911 = document.getElementById('imgPorsche911');
+
+function iniciarRepr(){
+    porsche911Audio.play();
+    console.log("Reproduciendo Sonido")
+}
+
+imgPorsche911.addEventListener('click',iniciarRepr);
